ae08dc0a3e8752081a54e347a3be1b52efdaa011
Ahd <dafuq.ahd@gmail.com>
2020-04-07 16:13:03 +0300

    import Provider and wrap it around Apps return jsx

--- djreact/frontend/src/components/App.js
+++ djreact/frontend/src/components/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./layout/Header";
 import Dashboard from "./products/Dashboard";
-
+import { Provider } from "react-redux";
+import store from "../store";
 const App = () => {
   return (
-    <>
-      <Header />
-      <Dashboard />
-    </>
+    <Provider store={store}>
+      <>
+        <Header />
+        <Dashboard />
+      </>
+    </Provider>
   );
 };
 
