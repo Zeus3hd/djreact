dafe1c2e04d7e3f3ea4f6488fb49234d748fd041
Ahd <dafuq.ahd@gmail.com>
2020-04-07 17:13:55 +0300

    create proptypes for Products component, connect products and map state to props

--- djreact/frontend/src/components/products/Products.js
+++ djreact/frontend/src/components/products/Products.js
@@ -1,9 +1,22 @@
 import React from "react";
-
-export default function Products() {
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getProducts } from "../../actions/products";
+function Products() {
   return (
     <div>
       <h1>Product List</h1>
     </div>
   );
 }
+
+Products.propTypes = {
+  products: PropTypes.array.isRequired,
+};
+const mapStateToProps = (state) => ({
+  // first products is by choice
+  // second products is the products reducer incombineReducer
+  // third products is the property in initiaState object in products reducer
+  products: state.products.products,
+});
+export default connect(mapStateToProps)(Products);
