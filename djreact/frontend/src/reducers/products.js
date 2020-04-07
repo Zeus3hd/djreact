5bd4c2f01886ff075f07679f76cc93998da8db9f
Ahd <dafuq.ahd@gmail.com>
2020-04-07 17:15:45 +0300

    change GET_LEADS to GET_PRODUCTS

--- djreact/frontend/src/reducers/products.js
+++ djreact/frontend/src/reducers/products.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case GET_LEADS:
+    case GET_PRODUCTS:
       return {
         ...state,
         products: action.payload,
