6f58fd92d3e7ee89703dd96b2fd4c47d2bd8f3c9
Ahd <dafuq.ahd@gmail.com>
2020-04-08 10:28:52 +0300

    create get user api to retrieve the logged in user

--- djreact/accounts/api.py
+++ djreact/accounts/api.py
@@ -32,3 +32,13 @@ class LoginAPI(generics.GenericAPIView):
         })
 
 # Get User API
+
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
