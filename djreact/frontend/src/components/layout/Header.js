8587a5a3cec957e25d2c9391c84d67bda4e953f7
Ahd <dafuq.ahd@gmail.com>
2020-04-08 13:22:27 +0300

    retrieve auth state from auth reducer and check if user is authenticated and render accordingly

--- djreact/frontend/src/components/layout/Header.js
+++ djreact/frontend/src/components/layout/Header.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 export default function Header() {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">login</Link>
-        </li>
-      </ul>
-    </div>
+  const auth = useSelector((state) => state.auth);
+  console.log(auth);
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to="/">home</Link>
+      </li>
+      <li>
+        <button>Logout</button>
+      </li>
+    </ul>
   );
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/">home</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">login</Link>
+      </li>
+    </ul>
+  );
+  return <div>{auth.isAuthenticated ? authLinks : guestLinks}</div>;
 }
