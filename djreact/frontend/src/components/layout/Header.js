f7629cca793ee8043f20cf99be3bf6a055bd9432
Ahd <dafuq.ahd@gmail.com>
2020-04-08 11:09:22 +0300

    create nav menu with links that leads to components

--- djreact/frontend/src/components/layout/Header.js
+++ djreact/frontend/src/components/layout/Header.js
@@ -1,4 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function Header() {
-  return <div>thisaaa</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">login</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
